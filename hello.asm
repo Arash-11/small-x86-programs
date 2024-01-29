7098dc7a3e7f2e65266aee5265e325d22fa192ad
Arash-11 <arashnawy@gmail.com>
2024-01-28 21:33:04 -0500

    First commit

---
+++ hello.asm
@@ -0,0 +1,15 @@
+         global _main
+
+         section .text
+_main:   mov     rax, 0x02000004 ; system call for write
+         mov     rdi, 1          ; file handle 1 is stdout
+         mov     rsi, message    ; address of string to output
+         mov     rdx, 13         ; number of bytes
+         syscall
+         mov     rax, 0x02000001 ; system call for exit
+         xor     rdi, rdi        ; exit code 0
+         syscall
+
+         ; stores variables, etc. - stuff we want in memory during the runtime of the program
+         section .data
+message: db      "Hello, World", 10
